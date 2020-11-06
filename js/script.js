0b194cfe8594d1d2517284c287640deb9fa589fe
Alex <alex.soderberg97@gmail.com>
2020-11-06 13:50:13 +0100

    now disables all buttons until game starts

--- js/script.js
+++ js/script.js
@@ -11,16 +11,33 @@ let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
 let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
 let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
 
+//eventlisteners for start game button
+const clickToStart = document.querySelector("#startGameBtn");
+clickToStart.addEventListener("click", startGame);
+
+//eventliseners for letter buttons
+const clickLetter = document.querySelectorAll("#letterButtons button");  
+for (let i = 0; i < clickLetter.length; i++) {
+    const button = clickLetter[i];
+    button.addEventListener("click", () => guessLetter(button.value, selectedWord));   
+} //  clickLetter.forEach(button => button.addEventListener("click", () => guessLetter(button.value, selectedWord)))); 
+
+clickLetter.forEach(button => button.disabled = true); // Avaktiverar knapparna så de inte kan användas innan man startar spelet
+
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
 function startGame (){
     selectedWord = randomWord();
     console.log(selectedWord);
-
+    activateLetterButtons(clickLetter);
     createBlanks(selectedWord);
 }
 // Funktion som slumpar fram ett ord
 function randomWord (){
     return wordList[Math.floor(Math.random() * wordList.length)];
+}
+// Funktion som aktiverar knapparna så att man kan gissa ordet
+function activateLetterButtons(clickLetter){
+    clickLetter.forEach(button => button.disabled = false);
 }
 // Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
 function createBlanks(word){
@@ -51,20 +68,8 @@ function guessLetter(letter, word){
 }
 // Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
-function activateLetterButtons(){
 
-}
 
-//eventlisteners for start game button
-const clickToStart = document.querySelector("#startGameBtn");
-clickToStart.addEventListener("click", startGame);
-
-//eventliseners for letter buttons
-const clickLetter = document.querySelectorAll("#letterButtons button");  
-for (let i = 0; i < clickLetter.length; i++) {
-    const button = clickLetter[i];
-    button.addEventListener("click", () => guessLetter(button.value, selectedWord));   
-}
-// clickLetter.forEach(element => element.addEventListener("click", () => guessLetter(element.value, selectedWord))));                   
+                  
 
 
